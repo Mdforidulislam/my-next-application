eb3010220648109ca9156eddc96198bbb60a64f0
Mdforidulislam <mdforidulislam962@gmail.com>
2024-01-29 00:16:17 +0600

    set nex js and router etc..

---
+++ src/app/blog/[id]/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const blogContent = ({params,searchParams}) => {
+    console.log(searchParams);
+    console.log(params);
+    return (
+        <div>
+            <h1>this is my pages </h1>
+        </div>
+    );
+};
+
+export default blogContent;
---
+++ src/app/blog/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mypages = ({params}) => {
+    console.log(params);
+    return (
+        <div>
+            <h1>my pages here </h1>
+        </div>
+    );
+};
+
+export default Mypages;
